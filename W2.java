d6e8e7b898b7bdda54cef9efd26ab7a67db8a56a
tpsparta <tprior@spartaglobal.com>
2026-08-30 13:18:50 +0100

    Create W2.java

---
+++ W2.java
@@ -0,0 +1,22 @@
+MCQ
+How to form a class
+Public class Sample { 
+Unit tests via JUnit
+Assertions
+Test-first development - tests can give clues about code, a black-box documentation e.g. expected parameters
+Arrays - used a lot in loops
+Methods
+Many questions - have a look at code, what will happen? Knowledge of syntax
+Method overloading - examples
+Forming classes, constructors (public ClassName)
+Extending classes (extends…)
+Overriding methods (@Override)
+Inheritance (abstract or normal class)
+Extending interactions with constructors (super(), equals, ==)
+Memory model - if a reference type value is deleted, 
+Garbage collector will remove references in memory, and then compress memory to allow for large
+String pool
+Parameterised tests - value source, csvsource
+4 pillars of object-oriented programming - how to define polymorphism
+Arithmetic operators: +-*/ modulus
+Abstract methods - final
